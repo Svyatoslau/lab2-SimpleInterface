6264c3f994f3792a9030d692f0df5d0e38307a09
Svyatoslav Yaroshevich <svyatyar2@gmail.com>
2020-11-03 23:04:37 +0300

    FIX:calculate1,calculate2'

--- src/bsu/rfe/java/group10/lab2/Yaroshevich/varC2/MainFrame.java
+++ src/bsu/rfe/java/group10/lab2/Yaroshevich/varC2/MainFrame.java
@@ -41,7 +41,7 @@ public class MainFrame extends JFrame {
 
     //Формула №1 для расчёта
     public Double calculate1(Double x,Double y,Double z){
-        return Math.pow((2*Math.log(1+x)+Math.cos(Math.PI*Math.pow(z,3))),Math.sin(y))+Math.pow(Math.pow(Math.E,Math.pow(x,2))+Math.cos(Math.pow(Math.E,z))+Math.pow(1/y,1/2),1/x);
+        return Math.pow((Math.log(Math.pow((1+x),2))+Math.cos(Math.PI*Math.pow(z,3))),Math.sin(y))+Math.pow(Math.pow(Math.E,Math.pow(x,2))+Math.cos(Math.pow(Math.E,z))+Math.pow(1/y,0.5),1/x);
     }
 
     // Считывания картинки и вставка её в label
@@ -59,7 +59,7 @@ public class MainFrame extends JFrame {
 
     //Формлуа №2 для рассчёта
     public Double calculate2(Double x,Double y,Double z){
-        return Math.pow(Math.cos(Math.PI*Math.pow(x,3))+2*Math.log(1+y),1/4)*(Math.pow(Math.E,Math.pow(z,2))+Math.pow(1/x,1/2)+Math.cos(Math.pow(Math.E,y)));
+        return Math.pow(Math.cos(Math.PI*Math.pow(x,3))+Math.log(Math.pow((1+y),2)),0.25)*(Math.pow(Math.E,Math.pow(z,2))+Math.pow(1/x,0.5)+Math.cos(Math.pow(Math.E,y)));
     }
 
     //методы помощники для добавления радио кнопок
